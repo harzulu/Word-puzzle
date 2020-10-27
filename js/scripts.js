99444a4a885bd2b2fc87484be5003cd59e43353c
Benjamin McFarland <benrmcfarland@gmail.com>
2020-10-27 13:09:19 -0700

    Make string split to change vowels
    Co-Authored-by:Amanda Otero <mandystar018@gmail.com>
    Co-authored-by: Joe Nero <josephnero111@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
-$(document).ready(function() {
+function isVowel(letter) {
+  let l = letter.toUpperCase();
+  if (l === "A" || l === "E" || l === "I" || l === "O" || l === "U") {
+    return true;
+  } else {
+    return false;
+  }
+}
 
+$(document).ready(function() {
+  $("#form").submit(function(event){
+    event.preventDefault();
+    let sentence = $("#string").val();
+    let array = sentence.split("");
+    alert(array);
+    for (let i = 0; i < array.length; i += 1) {
+      if (isVowel(array[i])) {
+        array[i]="-";
+      }
+    }
+    alert(array);
+  });
 });
